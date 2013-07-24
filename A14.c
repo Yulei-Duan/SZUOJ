bc642fbf91379cdf4a85de073f91961a09aefc34
Levi. <suiyun@me.com>
2013-07-24 18:11:45 +0800

    Create A14.c

---
+++ A14.c
@@ -0,0 +1,23 @@
+// author : Levi  7.24
+
+#include <stdio.h>
+#include <math.h>
+int A[102];
+int main()
+{
+
+  	int t, n, m, i;
+		double d;
+		scanf("%d", &t);
+		while(t--){
+  			scanf("%d%d", &n, &m);
+  			for (int i = 0; i < n; ++i){
+  				  d = floor(m/n);
+  				  A[i] = (m%n + d);
+  				  printf("%d ", A[i]);
+  				  m-=A[i];
+  			}
+			  printf("\n%d\n", m);
+		}
+		return 0;
+}	
